2c44cb7b6274a88f3413c4580b086e3b775bb493
jazzplaza <51905950+jazzplaza@users.noreply.github.com>
2026-04-17 17:19:10 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,79 @@
 $(function () {
-    $('.genealogy-tree ul').hide();
-    $('.genealogy-tree>ul').show();
-    $('.genealogy-tree ul.active').hide();
-    $('.genealogy-tree li').on('click', function (e) {
-        var children = $(this).find('> ul');
-        if (children.is(":visible")) children.hide('fast').removeClass('active');
-        else children.show('fast').addClass('active');
-        e.stopPropagation();
+    var $infoModal = $('#genealogy-info-modal');
+    var $infoTrigger = $('#genealogy-info-trigger');
+
+    function initGenealogyTree($tree) {
+        $tree.find('ul').hide();
+        $tree.children('ul').show();
+        $tree.find('ul.active').hide();
+    }
+
+    function syncMaternalTree() {
+        var $paternalBody = $('#genealogy-panel-paternal .genealogy-body').first();
+        var $maternalPanel = $('#genealogy-panel-maternal');
+
+        if (!$paternalBody.length || !$maternalPanel.length) {
+            return;
+        }
+
+        $maternalPanel.empty().append($paternalBody.clone());
+        initGenealogyTree($maternalPanel.find('.genealogy-tree'));
+    }
+
+    function setGenealogyPanel(panelName) {
+        $('[data-genealogy-tab]').each(function () {
+            var isActive = $(this).data('genealogyTab') === panelName;
+            $(this).toggleClass('is-active', isActive).attr('aria-selected', String(isActive));
+        });
+
+        $('[data-genealogy-panel]').each(function () {
+            var isActive = $(this).data('genealogyPanel') === panelName;
+            $(this).toggleClass('is-active', isActive).prop('hidden', !isActive);
+        });
+    }
+
+    function openInfoModal() {
+        $infoModal.prop('hidden', false);
+        $infoTrigger.attr('aria-expanded', 'true');
+    }
+
+    function closeInfoModal() {
+        $infoModal.prop('hidden', true);
+        $infoTrigger.attr('aria-expanded', 'false');
+    }
+
+    $('.genealogy-tree').each(function () {
+        initGenealogyTree($(this));
+    });
+
+    syncMaternalTree();
+
+    $(document).on('click', '.genealogy-tree li', function (event) {
+        var $children = $(this).children('ul');
+
+        if (!$children.length) {
+            return;
+        }
+
+        $children.stop(true, true).slideToggle('fast').toggleClass('active');
+        event.stopPropagation();
+    });
+
+    $('[data-genealogy-tab]').on('click', function () {
+        setGenealogyPanel($(this).data('genealogyTab'));
+    });
+
+    $infoTrigger.on('click', function () {
+        openInfoModal();
+    });
+
+    $infoModal.on('click', '[data-modal-close="true"]', function () {
+        closeInfoModal();
+    });
+
+    $(document).on('keydown', function (event) {
+        if (event.key === 'Escape' && !$infoModal.prop('hidden')) {
+            closeInfoModal();
+        }
     });
 });
